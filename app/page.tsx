2544661263fe28c0a77b5793e8f0d5d3e991d5ee
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-08-02 14:56:29 -0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+
+export default function Home() {
+
+  const habits = {
+    'Estudar': {
+      '2023-08-01': true,
+      '2023-07-31': false,
+      '2023-07-30': false,
+    },
+    'Academia': {
+      '2023-08-01': false,
+      '2023-07-31': false,
+      '2023-07-30': true,
+    }
+  };
+
+  return (
+    <main className="container relative flex flex-col gap-8 px-4 pt-16">
+      {
+        habits === null || Object.keys(habits).length === 0 && (
+          <h1 className="mt-20 text-4xl font-light text-white font-display text-center">
+            Você não tem Tarefas cadastradas!
+          </h1>
+        )
+      }
+      {
+        habits !== null && Object.entries(habits).map(([habit, sequence]) => (
+          <div key={habit}>
+            <div className="flex justify-between items-center">
+              <span className="text-xl font-light text-white font-sans">{habit}</span>
+              <button>delete</button>
+            </div>
+          </div>
+        ))
+      }
+    </main>
+  )
+}
